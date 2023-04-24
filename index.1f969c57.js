d9fbfcb3368141acbcb6f05e6a52b6ac33a8441a
evgeniy210191 <88433785+evgeniy210191@users.noreply.github.com>
2023-04-24 21:18:00 +0000

    Deploying to gh-pages from @ evgeniy210191/goit-js-hw-10@634727ea7521e47d9af32846689a71dad6c0e615 🚀

--- index.1f969c57.js
+++
@@ -1,2 +0,0 @@
-const[o,n,e]=[".country-list",".country-info","#search-box"].map((o=>document.querySelector(o)));var t;(t="https://restcountries.com/v3.1/all?fields=name,flags",fetch(t).then((o=>o.json()))).then((function(o){return console.log(o),o.map((o=>{console.log(o.flags.png)}))}));
-//# sourceMappingURL=index.1f969c57.js.map
